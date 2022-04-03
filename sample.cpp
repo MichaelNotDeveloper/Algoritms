0c129f8b31e2f0156d87302250dd0dfc8fe6f06d
Michael Zameshaev <michaelzamesh@mail.ru>
2022-04-03 12:04:09 +0700

    Basic file added

---
+++ sample.cpp
@@ -0,0 +1,52 @@
+#pragma GCC optimize("O2")
+#include "bits/stdc++.h"
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define MOD ll(998244353)
+#define INF ll(1e18)
+#define SIZE 2000000
+
+#define pb push_back
+#define ppb pop_back
+#define f first
+#define s second
+
+#define all(x) x.begin(), x.end()
+#define vout(x) for(auto u : x)cout << u << ' ';
+#define rndm drist(rng)
+
+#define loop(i, n) for(ll i = 0; i < n;i++)
+#define loopone(i, n) for(ll i = 1; i <= n;i++)
+#define loopb(i, n) for(ll i = n-1; i >= 0;i--)
+#define loopbone(i, n) for(ll i = n; i > 0;i--)
+
+mt19937_64 rng(time(0));
+uniform_int_distribution<ll> drist;
+
+ll n,m,k,i,j,x,y,z,w;
+string str, trash;
+char chr;
+ll sussy_baka;
+
+//----------///
+
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    #ifdef DEBUG
+    freopen("test.txt", "r", stdin);
+    #endif
+    
+    // -------- //
+      
+    return 0;
+}
+    
+
